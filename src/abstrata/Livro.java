5306a0ff66ee08d95a71349c3cf9e24681e195a5
Miguel Farias <miguelfarias@alu.ufc.br>
2022-11-18 15:10:16 -0300

    Adicionei a classe abstrata Livro.

---
+++ src/abstrata/Livro.java
@@ -0,0 +1,32 @@
+package abstrata;
+
+public abstract class Livro
+{
+    private String titulo;
+    private String editora;
+    private String isbn;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getEditora() {
+        return editora;
+    }
+
+    public void setEditora(String editora) {
+        this.editora = editora;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+}
